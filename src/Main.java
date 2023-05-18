4b2493a5ee899be4e2f7bbfdeed2f62b399a8b04
kaileekuan <kaileekuan@gmail.com>
2023-05-17 17:05:39 -0700

    Shifted the tree to desired location on the canvas. Made a second tree that has a random angle and length to vary the shape of the tree from the first tree. Also made a function that calls a random number of branches as well. Tree2 works as intended in the middle of the console, next to the initial tree.

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,39 @@
 import processing.core.PApplet;
-public class Main extends PApplet{
+public class Main extends PApplet {
 
     private float theta;
+
+    private float theta2;
+
 
     public static void main(String[] args) {
         PApplet.main("Main");
     }
 
     public void settings() {
-        size(800, 800); //draw the canvas
+        size(600, 600); //draw the canvas
     }
 
     public void setUp() {
     }
 
-    public void draw(){
+    public void draw() {
         background(255);
-        theta = map(mouseX,0,width,0,PI/2);
+        theta = map(mouseX, 0, width, 0, PI / 2);
+        theta2 = map(mouseX, 0, width, 0, PI / 2);
 
-        translate(width/2, height);
+        translate(width / 4, height / 3);
         stroke(0);
-        branch(60);
+        tree1(60);
+        strokeWeight(2);
+
+        translate(width / 3, height / 8);
+        stroke(0);
+        tree2(80);
+        strokeWeight(2);
     }
 
-    public void branch(float len){
+    public void tree1(float len) { //tree number 1: basic tree on top left console
         line(0, 0, 0, -len);
         translate(0, -len);
 
@@ -32,17 +42,39 @@ public class Main extends PApplet{
         if (len > 2) {
             pushMatrix();
             rotate(theta);
-            branch(len);
+            tree1(len);
             popMatrix();
 
             pushMatrix();
             rotate(-theta);
-            branch(len);
+            tree1(len);
             popMatrix();
         }
-        }
     }
- //move tree to different position
+
+    public void tree2(float len2) { //tree number 2: tree with random angle and length on top middle console and also random branches
+        float theta2 = random(0, PI / 2);
+        line(0, 0, 0, -len2);
+        translate(0, -len2);
+        len2 *= 0.66;
+
+        if (len2 > 2) {
+            int n = (int) (Math.random() * 4) + 1;
+            for (int i = 0; i < n; i++) {
+                float theta3 = random(-PI / 2, PI / 2);
+                pushMatrix();
+                rotate(theta3);
+                tree2(len2);
+                popMatrix();
+            }
+        }
+
+
+    }
+}
+
+
+//add different features like lightening and mountains 
 
     //start w fractal trees + flowers
     //if time, add L-systems
