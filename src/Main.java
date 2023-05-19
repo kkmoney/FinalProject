ff29270113d0dc4fbfe75fbb45224bc2ef5ae87e
kaileekuan <kaileekuan@gmail.com>
2023-05-19 16:43:43 -0700

    First I made a drawing made out of four different recursive circles. Second, I made a rainbow tunnel drawing that changes based on the mouse's location (doesn’t work yet). Finally, I made a rain particle animation that makes rain fall from the sky (does not work yet). I made all three methods inside keypressed too.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 import processing.core.PApplet;
+import processing.core.PVector;
+import java.util.ArrayList;
+
 public class Main extends PApplet {
-  private float theta;
+    private float theta;
     private double x = 0.0;
     private double y = 0.0;
-
-    private String timestamp; //variable for naming saved frames
     private float theta2;
     private float counter = 0;
-    private float noPetals = 4; //number of petals the fractal flower has
-    private float reductionFactor = 0.66F; //by what factor should the next branch get reduced by? 0.5 = each branch is half the size of the previous one.
-    private float cutOff = 7; //what is the minimum branch length?  the fractal will continue to be drawn until this minimum size is reached.
-    private float branchFactor = 2 ; //number of branches at each level
+    private float petals = 4; //number of petals the fractal flower has
+    private float reductionFactor = 0.66F; //by what factor should the next branch get reduced by
+    private float cutOff = 7; //minimum branch length
+    private float branch = 2 ; //number of branches at each level
+
+    private PVector start;
+    private PVector end;
+    private ArrayList<kochSnowflake> lines;
     public static Main app;
 
     public Main(){
@@ -31,41 +36,41 @@ public class Main extends PApplet {
 
     public void draw() {
         background(255);
-        theta = map(mouseX, 0, width, 0, PI / 2);
-
-        translate(width / 2, height / 1);
-        strokeWeight(4);
         keyPressed();
 
+    }
+
+
+    public void drawFlower(){
         counter += 1;
         theta2 = radians(counter);
         stroke(0,255,0);
-        translate(width/400, height/100);
-        for(int i = 0; i < noPetals; i++) {
+        translate((float) (width / 2), (float) (height / 2));
+        for(int i = 0; i < petals; i++) {
             drawPetal();
-            rotate((float) (PI / (noPetals/2.0)));
+            rotate((float) (PI / (petals / 2.0)));
         }
     }
 
     public void branch(float h) {
         h *= reductionFactor;
         if (h > cutOff) {
-            for(int i = 0; i < branchFactor; i++) {
+            for(int i = 0; i < branch; i++) {
                 pushMatrix();
-                rotate(branchFactor*theta2/2);
+                rotate(branch*theta2/2);
                 line(0, 0, 0, -h);
                 translate(0, -h);
                 branch(h);
                 popMatrix();
 
                 pushMatrix();
-                rotate(-branchFactor*theta2/2);
+                rotate(-branch*theta2/2);
                 line(0, 0, 0, -h);
                 translate(0, -h);
                 branch(h);
                 popMatrix();
 
-                if (branchFactor % 2 != 0) {
+                if (branch % 2 != 0) {
                     pushMatrix();
                     line(0, 0, 0, -h);
                     translate(0, -h);
@@ -83,7 +88,7 @@ public class Main extends PApplet {
         translate(0, height/8);
     }
 
-        public void tree1(float len) { //tree number 1
+    public void tree1(float len) { //tree number 1
         line(0, 0, 0, -len);
         translate(0, -len);
 
@@ -103,15 +108,38 @@ public class Main extends PApplet {
         }
     }
 
-    public void keyPressed(){
-        if(key == 'k'){
+    public void keyPressed() {
+        if (key == 'a') {
+
+            theta = map(mouseX, 0, width, 0, PI / 2); //angle of big recursive tree
+            translate(width / 2, height / 1);
+            strokeWeight(4);
             tree1(150);
+
+        } else if (key == 'b') {
+
+            drawFlower(); //make flower appear
+
+        } else if (key == 'c') {;
+
+            //how do i call the class kochsnowflake?
+            //make fractal snowflakes (koch) appear
+
+        }  else if (key == 'd') {
+
+            //makes the snowflake spin
+        } else if (key == 'e') {
+            //square fractal appear
+        } else if (key == 'f') {
+            //make square spin
+        } else if (key == 'g') {
+            //make circle appear
+        } else if (key == 'h') {
+            //make circles spin
         }
+
+    }
     }
 
-    //with each key pressed, a fractal snowflakes (koch) appears
-    //another key pressed makes the snowflake spin
-    //with another key pressed, make a square fractal appear
-    //another key pressed makes the square spin
+    //implement screen with instructions
 
-}
